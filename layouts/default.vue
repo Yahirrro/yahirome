d36a45a58ab98cdba33b28a12aebac4c88b0a064
yahirrro <yahiro0921@gmail.com>
2020-01-09 04:51:35 +0900

    design fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,19 @@
   <div>
     <h1 class="__top__right__title">星乃やひろ</h1>
     <nuxt-link to="/" aria-label="Go to Top">
-
-      <lottie class="__top__left__logo" :options="logo" :height="30" :width="30" v-on:animCreated="handleAnimation"/>
-
+      <lottie
+        class="__top__left__logo"
+        :options="logo"
+        :height="30"
+        :width="30"
+        v-on:animCreated="handleAnimation"
+      />
     </nuxt-link>
     <navbar />
     <transition name="page">
       <nuxt />
     </transition>
-    <mainfooter v-if="$nuxt.$route.name !== 'index'" />
+    <mainfooter v-if="$nuxt.$route.path != '/'" />
   </div>
 </template>
 <style lang="scss">
@@ -37,22 +41,20 @@ export default {
   },
   head() {
     return {
-      script: [
-        { src: "/typekit.js" },
-      ]
+      script: [{ src: "/typekit.js" }]
     };
   },
   data() {
     return {
       // データを定義
       logo: { animationData: main_logo, loop: false },
-      animationSpeed: 1,
+      animationSpeed: 1
     };
   },
   methods: {
     handleAnimation: function(anim) {
       this.anim = anim;
-    },
+    }
   }
 };
 </script>
