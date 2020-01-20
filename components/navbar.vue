046b0bb13d3cd8feae295f2f2ab9876a2e11afed
yahirrro <yahiro0921@gmail.com>
2020-01-20 16:10:12 +0900

    added bottom navbar

--- components/navbar.vue
+++ components/navbar.vue
@@ -37,6 +37,24 @@
       </nuxt-link>
     </div>
     <div class="nav-main__a-box">
+      <a href="https://blog.yahiro.me" target="_blank">
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 576 512"
+        >
+          <path
+            fill="currentColor"
+            d="M144 64A144 144 0 0 0 0 208v208a32 32 0 0 0 32 32h256V208A144 144 0 0 0 144 64zm80 176a16 16 0 0 1-16 16H80a16 16 0 0 1-16-16v-32a16 16 0 0 1 16-16h128a16 16 0 0 1 16 16zM432 64H244.87C290.23 95.87 320 148.48 320 208v240h224a32 32 0 0 0 32-32V208A144 144 0 0 0 432 64zm80 208a16 16 0 0 1-16 16h-32a16 16 0 0 1-16-16v-48h-56a8 8 0 0 1-8-8v-16a8 8 0 0 1 8-8h104a16 16 0 0 1 16 16z"
+            class
+          />
+        </svg>
+        <br />Blog
+      </a>
+    </div>
+    <div class="nav-main__a-box">
       <nuxt-link to="/contact">
         <svg
           aria-hidden="true"
